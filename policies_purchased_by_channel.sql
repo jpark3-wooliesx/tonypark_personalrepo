59973ca0ec98a7bb46ba920c238c8065ecf39988
jpark3 <jpark3@woolworths.com.au>
2025-04-02 10:48:56 +1100

    add adobe to bq policy id matching

--- policies_purchased_by_channel.sql
+++ policies_purchased_by_channel.sql
@@ -147,3 +147,79 @@ from channel_assignment
 group by 1,2
 order by 1,2
 ;
+
+
+--Adobe and BigQuery Policy ID Matching
+with
+dates as (
+  select
+  date('2024-11-01') as start_date
+  ,date('2025-01-31') as end_date
+)
+,bq_policies as (
+  select distinct
+  lower(SourceProduct) as product
+  ,case when SourceProduct in ('CAR','HOME') then substr(PolicyIdentifier,4,100) else PolicyIdentifier end as pid
+  from gcp-wow-rwds-ai-insurance-prod.analytics_layer.master_data p
+  join dates d
+    on p.PolicyPurchaseDate between d.start_date and d.end_date
+  where (SourceProduct = 'CAR' or (SourceProduct = 'HOME' and ProductSubType in ('Combined Home & Contents','Content Only Cover','Home Only Cover')) or SourceProduct = 'TRAVEL')
+)
+,grs_policies as (
+  select
+  * except(hit_num)
+  from (
+    select
+    lower(eVar7) as product
+    ,evar61 as pid
+    ,mcvisid as adobe_id
+    ,date_time
+    ,visit_num
+    ,m.channel_name
+    ,va_closer_detail as channel_detail
+    ,row_number() over(partition by evar61 order by date_time) as hit_num
+    ,visit_referrer
+    ,post_evar46 as utm_param
+    ,pagename
+    from gcp-wow-food-wlx-digaspt-dev.prod_adobe_data.grs_adobe a
+    join dates d
+      on date(a.date_time) between d.start_date and d.end_date
+    left join gcp-wow-food-wlx-digaspt-dev.prod_adobe_data.dim_grs_marketing_channel m
+      on cast(a.va_closer_id as numeric) = m.channel_id
+    where 1=1
+    and evar61 is not null
+    and evar7 in ('car','home','landlord','pet','travel')
+    and pagename in ('ev-ins:car:quote:thanks','ev-ins:home:quote:thanks','ev-ins:quoteline:travel:thankyou')
+  )
+  where hit_num = 1 --only take the first hit for purchase
+)
+,callcentre_policies as (
+  select 'car' as product
+  ,substr(policy_id,4,100) as pid
+  from gcp-wow-rwds-ai-insurance-dev.tony.callcentre_car_novjan
+
+  union all
+
+  select 'home' as product
+  ,substr(policy_id,4,100) as pid
+  from gcp-wow-rwds-ai-insurance-dev.tony.callcentre_home_novjan
+)
+select
+coalesce(b.product,c.product,g.product) as product
+,sum(case when b.pid is not null then 1 else 0 end) as bq_sales
+,sum(case when b.pid is not null and c.pid is null then 1 else 0 end) as bq_nonhub_sales
+,sum(case when b.pid is not null and c.pid is not null then 1 else 0 end) as bq_hub_sales
+,sum(case when g.pid is not null then 1 else 0 end) as grs_sales
+,sum(case when b.pid is not null and g.pid is not null then 1 else 0 end) as both_sales
+,sum(case when b.pid is not null and c.pid is null and g.pid is not null then 1 else 0 end) as nonhub_both_sales
+,sum(case when b.pid is not null and g.pid is null then 1 else 0 end) as bq_only_sales
+,sum(case when b.pid is not null and c.pid is null and g.pid is null then 1 else 0 end) as bq_nonhub_only_sales
+,sum(case when b.pid is null and g.pid is not null then 1 else 0 end) as grs_only_sales
+from bq_policies b
+left join callcentre_policies c
+on b.pid = c.pid
+full outer join grs_policies g
+on b.pid = g.pid and b.product = g.product
+group by 1
+order by 1
+;
