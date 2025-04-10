a48acb997add39c9804de07b4db8b2fb8bcb7b88
jpark3 <jpark3@woolworths.com.au>
2025-04-10 16:49:29 +1000

    add method using analytics_layer grs table

--- grs_gcp/retention_rate_by_channel.sql
+++ grs_gcp/retention_rate_by_channel.sql
@@ -1,4 +1,93 @@
---Weekly: Retention Rate by Custom Insurance Segments
+--Monthly: Retention Rate by Custom Insurance Segments
+
+declare report_start date default date('2024-03-01');
+declare report_end date default current_date('Australia/Sydney')-1;
+
+with
+retention_calcs as (
+  select
+  *
+  ,case
+    when date_diff(report_end,start_date,DAY) >= coolingoff_days
+      then if(date_diff(end_date,start_date,DAY) <= coolingoff_days,1,0)
+    else null
+  end as coolingoff_cancelled
+  ,case
+    when date_diff(report_end,start_date,DAY) >= 30
+      then if(date_diff(end_date,start_date,DAY) <= 30,1,0)
+    else null
+  end as mtc_30d
+  ,case
+    when date_diff(report_end,start_date,DAY) >= 60
+      then if(date_diff(end_date,start_date,DAY) <= 60,1,0)
+    else null
+  end as mtc_60d
+  ,case
+    when date_diff(report_end,start_date,DAY) >= 90
+      then if(date_diff(end_date,start_date,DAY) <= 90,1,0)
+    else null
+  end as mtc_90d
+  ,case --adjust for cooling-off
+    when report_end > first_renewal_date_plus_coolingoff and end_date >= first_renewal_date
+      then if(end_date > first_renewal_date_plus_coolingoff,1,0)
+    else null
+  end as renewed
+  ,case --adjust for cooling-off
+    when report_end > first_renewal_date_plus_coolingoff
+      then if(end_date > first_renewal_date_plus_coolingoff,1,0)
+    else null
+  end as retained
+  from (
+    select
+    *
+    ,date_add(first_renewal_date, interval coolingoff_days DAY) first_renewal_date_plus_coolingoff
+    from (
+      select
+      lower(SourceProduct) as product
+      ,policypurchasedateestimated as purchase_date
+      ,policystartdateestimated as start_date
+      ,date_trunc(policystartdateestimated,month) as start_month
+      ,coalesce(policyenddateestimated,date('2099-12-31')) as end_date
+      ,policyrenewaldateestimated as recorded_renewal_date
+      ,PolicyCoolingOffPeriodDays as coolingoff_days
+      ,date_add(policyrenewaldateestimated, INTERVAL -(extract(year from policyrenewaldateestimated) - extract(year from policystartdateestimated)) +1 YEAR) as first_renewal_date
+      ,g.utm_param
+      ,g.custom_channel
+      ,g.default_channel
+      ,g.default_channel_detail
+      from gcp-wow-rwds-ai-insurance-prod.analytics_layer.master_data p
+      join gcp-wow-rwds-ai-insurance-dev.analytics_layer.grs_policy_orders g
+        on p.PolicyIdentifier = g.pid
+      where p.policystartdateestimated between report_start and report_end
+      and SourceProduct in ('CAR','HOME')
+    )
+  )
+)
+select
+product
+,start_month
+,custom_channel
+,count(*) purchase_cnt
+,sum(coolingoff_cancelled) coolingoff_cancelled
+,sum(mtc_30d) mtc_30d
+,sum(mtc_60d) mtc_60d
+,sum(mtc_90d) mtc_90d
+,sum(renewed) renewed
+,sum(retained) retained
+,sum(coolingoff_cancelled)*1.0/count(coolingoff_cancelled) coolingoff_cancel_rate
+,sum(mtc_30d)*1.0/count(mtc_30d) mtc_30d_rate
+,sum(mtc_60d)*1.0/count(mtc_60d) mtc_60d_rate
+,sum(mtc_90d)*1.0/count(mtc_90d) mtc_90d_rate
+,sum(renewed)*1.0/count(renewed) renewal_rate
+,sum(retained)*1.0/count(retained) retention_rate
+from retention_calcs
+group by 1,2,3
+order by 1,2,3
+;
+
+
+
+--[Manual] Weekly: Retention Rate by Custom Insurance Segments
 with
 date_range as (
   select
@@ -184,7 +273,7 @@ group by 1,2,3
 order by 1,2,3
 ;
 
---Monthly: Retention Rate by Custom Insurance Segments
+--[Manual] Monthly: Retention Rate by Custom Insurance Segments
 with
 date_range as (
   select
